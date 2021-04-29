db917d58649f90a099ef2e175e37c008f62db80d
Ken Sodemann <ken@ionic.io>
2021-04-29 13:21:39 -0500

    step(three): specify different auth types

--- src/app/core/vault.service.ts
+++ src/app/core/vault.service.ts
@@ -4,7 +4,7 @@ import {
   AuthMode,
   IonicNativeAuthPlugin,
 } from '@ionic-enterprise/identity-vault';
-import { Platform } from '@ionic/angular';
+import { NavController, Platform } from '@ionic/angular';
 import { Session } from '../models';
 import { BrowserVaultPlugin } from './browser-vault.plugin';
 
@@ -12,16 +12,36 @@ import { BrowserVaultPlugin } from './browser-vault.plugin';
   providedIn: 'root',
 })
 export class VaultService extends IonicIdentityVaultUser<Session> {
+  private currentSession: Session | undefined;
+
   constructor(
     private browserVaultPlugin: BrowserVaultPlugin,
+    private navController: NavController,
     platform: Platform,
   ) {
     super(platform, {
       unlockOnAccess: true,
       hideScreenOnBackground: true,
       lockAfter: 5000,
-      authMode: AuthMode.SecureStorage,
     });
+  }
+
+  login(session: Session, mode?: AuthMode): Promise<void> {
+    this.currentSession = session;
+    return super.login(session, mode);
+  }
+
+  async restoreSession(): Promise<Session | undefined> {
+    return this.currentSession || super.restoreSession();
+  }
+
+  onVaultLocked() {
+    this.currentSession = undefined;
+    this.navController.navigateRoot(['/', 'tabs', 'tab3']);
+  }
+
+  onSessionRestored(session: Session) {
+    this.currentSession = session;
   }
 
   getPlugin(): IonicNativeAuthPlugin {
